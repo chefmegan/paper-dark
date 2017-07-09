9d56a5e12747df1ac76ffca39886f335f317c197
lukio3 <lukemckaysmith@gmail.com>
2017-07-09 20:59:15 +0100

    Update paginate-index.blade.php

--- resources/views/frontend/blog/partials/paginate-index.blade.php
+++ resources/views/frontend/blog/partials/paginate-index.blade.php
@@ -1,38 +1,30 @@
-<ul class="pager">
+<div>
     {{-- Reverse direction --}}
     @if ($reverse_direction)
         @if ($posts->currentPage() > 1)
-            <li class="previous">
-                <a href="{!! $posts->url($posts->currentPage() - 1) !!}">
+                <a class="button is-outlined" href="{!! $posts->url($posts->currentPage() - 1) !!}">
                     <i class="fa fa-angle-left fa-lg"></i>
                     Previous {{ $tag->tag }}
                 </a>
-            </li>
         @endif
         @if ($posts->hasMorePages())
-            <li class="next">
-                <a href="{!! $posts->nextPageUrl() !!}">
+                <a class="button is-outlined" href="{!! $posts->nextPageUrl() !!}">
                     Next {{ $tag->tag }}
                     <i class="fa fa-angle-right"></i>
                 </a>
-            </li>
         @endif
     @else
         @if ($posts->currentPage() > 1)
-            <li class="previous">
-                <a href="{!! $posts->url($posts->currentPage() - 1) !!}">
+                <a class="button is-outlined" href="{!! $posts->url($posts->currentPage() - 1) !!}">
                     <i class="fa fa-angle-left fa-lg"></i>
                     Newer {{ $tag ? $tag->tag : '' }}
                 </a>
-            </li>
         @endif
         @if ($posts->hasMorePages())
-            <li class="next">
-                <a href="{!! $posts->nextPageUrl() !!}">
+                <a class="button is-outlined" href="{!! $posts->nextPageUrl() !!}">
                     Older {{ $tag ? $tag->tag : '' }}
                     <i class="fa fa-angle-right"></i>
                 </a>
-            </li>
         @endif
     @endif
-</ul>
+</div>
