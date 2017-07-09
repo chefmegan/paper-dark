b19fb37ea20c29592c8a8b9fe0200b3901e1db45
lukio3 <lukemckaysmith@gmail.com>
2017-07-09 20:53:25 +0100

    Update css.blade.php

--- resources/views/frontend/shared/partials/css.blade.php
+++ resources/views/frontend/shared/partials/css.blade.php
@@ -1,5 +1,12 @@
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/themes/prism.min.css">
+
+
+
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/prism.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/components/prism-php.min.js"></script>
 <link rel="stylesheet" type="text/css" href="{{ elixir('vendor/canvas/assets/css/frontend.css') }}">
+
+<!-- font awesome -->
+<link rel="stylesheet" id="font-awesome" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<!-- bulma-->
+<link rel="stylesheet" id="bulma" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.2/css/bulma.min.css" />
