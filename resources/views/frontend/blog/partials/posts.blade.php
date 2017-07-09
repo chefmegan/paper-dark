8304ea4aeeabb764bcfa2d5edb38bf44431f2aba
lukio3 <lukemckaysmith@gmail.com>
2017-07-09 21:01:35 +0100

    Update posts.blade.php

--- resources/views/frontend/blog/partials/posts.blade.php
+++ resources/views/frontend/blog/partials/posts.blade.php
@@ -1,19 +1,54 @@
-@foreach ($posts as $post)
-    <div class="post-preview">
-        <h2 class="post-title">
-            <a href="{{ $post->url($tag) }}">{{ $post->title }}</a>
-        </h2>
-        <p class="post-meta">
-            {{ $post->published_at->diffForHumans() }} &#183; {{ $post->readingTime() }} MIN READ
-            <br>
-            @unless( $post->tags->isEmpty())
-                {!! implode(' ', $post->tagLinks()) !!}
-            @endunless
-        </p>
-        <p class="postSubtitle">
-            {{ str_limit($post->subtitle, config('blog.frontend_trim_width')) }}
-        </p>
-        <p style="font-size: 13px"><a href="{{ $post->url($tag) }}">READ MORE...</a></p>
+
+@foreach ($posts->chunk(3) as $chunk)
+
+<div class="columns">
+
+  @foreach ($chunk as $post)
+
+  <div class="column is-4">
+
+    <div class="card">
+      <header class="card-header">
+        <a class="card-header-title" href="{{ $post->url($tag) }}">
+          {{ $post->title }}
+        </a>
+        <span class="card-header-icon timestamp">
+          {{ $post->published_at->diffForHumans() }}
+        </span>
+      </header>
+
+      @if ($post->page_image)
+
+      <a href="{{ $post->url($tag) }}">
+        <div class="card-image">
+          <figure class="image is-4by4">
+            <img src="{{ asset($post->page_image) }}">
+          </figure>
+        </div>
+      </a>
+
+      @endif
+
+      <div class="card-content">
+
+    <div class="content">
+        {{ str_limit($post->subtitle, config('blog.frontend_trim_width')) }} <a href="{{ $post->url($tag) }}">read more</a>
+     <br>
+     @unless( $post->tags->isEmpty())
+     <span class="tag is-small">
+       {!! implode(' ', $post->tagLinks()) !!}
+     </span>
+     @endunless
+   </div>
+
+      </div>
+
     </div>
-    <hr>
+
+  </div>
+
+  @endforeach
+
+</div>
+
 @endforeach
