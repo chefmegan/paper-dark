4d8c8c37b7d99b58c09382201b250cd001701d5b
lukio3 <lukemckaysmith@gmail.com>
2017-07-09 20:51:37 +0100

    Update layout.blade.php

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -1,15 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        @include('canvas::frontend.shared.partials.meta')
-        @include('canvas::frontend.shared.partials.css')
-        @include('canvas::frontend.shared.partials.user-generated-css')
-    </head>
-    <body>
-        @include('canvas::frontend.shared.partials.header')
-        @yield('content')
-        @yield('unique-js')
-        @include('canvas::frontend.shared.partials.user-generated-js')
-        @include('canvas::frontend.shared.partials.footer')
-    </body>
+<head>
+
+  @include('layouts.css')
+ 
+  @include('canvas::frontend.shared.partials.meta')
+
+  @include('canvas::frontend.shared.partials.user-generated-css')
+
+</head>
+
+<body>
+
+      @include('canvas::frontend.shared.partials.header')
+      
+<section class="body-section">
+      
+      @yield('content')
+      @include('canvas::frontend.shared.partials.user-generated-js')
+      
+      @include('layouts.bottom')
+      @yield('unique-js')
+      @include('layouts.scripts')
+      
+<section>      
+
+</body>
+
 </html>
