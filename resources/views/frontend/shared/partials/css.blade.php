68ab586040b57d81206c75e22cfa1643f85f51fe
lukio3 <lukemckaysmith@gmail.com>
2017-07-10 21:16:29 +0100

    Update css.blade.php

--- resources/views/frontend/shared/partials/css.blade.php
+++ resources/views/frontend/shared/partials/css.blade.php
@@ -4,7 +4,6 @@
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/prism.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/components/prism-php.min.js"></script>
-<link rel="stylesheet" type="text/css" href="{{ elixir('vendor/canvas/assets/css/frontend.css') }}">
 
 <!-- font awesome -->
 <link rel="stylesheet" id="font-awesome" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
