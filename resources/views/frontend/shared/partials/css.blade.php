24d6d8849556428047afdfeebc71e1f6f458fc16
lukio3 <lukemckaysmith@gmail.com>
2017-07-10 21:03:56 +0100

    Update css.blade.php

--- resources/views/frontend/shared/partials/css.blade.php
+++ resources/views/frontend/shared/partials/css.blade.php
@@ -4,3 +4,11 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/components/prism-php.min.js"></script>
 <link rel="stylesheet" type="text/css" href="{{ elixir('vendor/canvas/assets/css/frontend.css') }}">
 
+<style>
+
+.container {
+  padding-top:50px;
+  max-width: 968px;
+}
+
+</style>
