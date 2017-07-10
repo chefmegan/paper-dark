59a7d888ff6b9b5c9e947fc3111d4c892990cbc0
lukio3 <lukemckaysmith@gmail.com>
2017-07-10 21:58:01 +0100

    Update footer.blade.php

--- resources/views/frontend/shared/partials/footer.blade.php
+++ resources/views/frontend/shared/partials/footer.blade.php
@@ -7,7 +7,7 @@
 </div>
 
 <br>
-<span id="top-link-block" class="hidden hover-button">
+<span class="hidden hover-button" id="top-link-block">
 <a id="scroll-to-top" class="tag is-primary is-large" href="#top">Scroll to Top</a>
 </span>
 
